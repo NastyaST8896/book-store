f8a6780027a82e6d76a8bf98eac51d7d5f6762f6
Anastasia Triapochkina <me@trjapochkina.ru>
2026-04-10 18:02:12 +0300

    getRecommendedBook in recommendations component

--- src/pages/product/elements/recommendations.tsx
+++ src/pages/product/elements/recommendations.tsx
@@ -1,19 +1,31 @@
-import { BookCard } from "@common/book-card";
-import { Box, Grid, Typography, useMediaQuery } from "@mui/material";
-import type { Book } from "@utils/types";
+import { BookCard } from '@common/book-card';
+import { Box, Grid, Typography, useMediaQuery } from '@mui/material';
+import type { Book } from '@utils/types';
 import { styled, useTheme } from '@mui/material/styles';
+import { useEffect, useMemo, useState } from 'react';
+import { useParams } from 'react-router';
+import { getCartBooks } from '@redux/cart-books/thunk';
+import { useAppDispatch } from '@redux/hooks';
+import { getRecommendedApi } from '../../../api/recommended-api';
+import type { CartBookType } from '@redux/cart-books/slice';
 
 type RecommendationsType = {
-  recommendedBooks: Book[]
+  cartBooks: CartBookType[];
 }
 
 export const Recommendations = (props: RecommendationsType) => {
-  const { recommendedBooks } = props;
+  const { cartBooks } = props
+
+  const dispatch = useAppDispatch();
+
+  const [recommended, setRecommended] = useState<Book[]>([]);
 
   const theme = useTheme();
   const mobile = useMediaQuery(theme.breakpoints.down('sm'));
   const tabletFrom = useMediaQuery(theme.breakpoints.down('md'));
   const tabletTo = useMediaQuery(theme.breakpoints.up('sm'));
+
+  const { id } = useParams();
 
   let limitSize: number = 0;
 
@@ -22,6 +34,42 @@ export const Recommendations = (props: RecommendationsType) => {
   } else if (tabletTo && tabletFrom) {
     limitSize = 3;
   }
+
+  useEffect(() => {
+    if (!id) {
+      return
+    }
+
+    const getRecommendedBook = async () => {
+      const result = await getRecommendedApi({ id });
+
+      if (result.recommended.length > 4) {
+        result.recommended.splice(4);
+      }
+
+      setRecommended(result.recommended);
+
+      return;
+    };
+
+    getRecommendedBook();
+
+    dispatch(getCartBooks());
+  }, [dispatch, id]);
+
+  const mergedRecommendedBooks = useMemo(() => {
+    return recommended.map((book) => {
+      const cartBook = cartBooks.find((bookInCart) => {
+        return bookInCart.id === book.id;
+      });
+
+      if (cartBook) {
+        return { ...book, count: cartBook.count };
+      }
+
+      return book;
+    });
+  }, [cartBooks, recommended]);
 
   return (
     <StyledBox>
@@ -32,7 +80,7 @@ export const Recommendations = (props: RecommendationsType) => {
         columnSpacing="20px"
       >
         {(limitSize !== 0) ? (
-          recommendedBooks
+          mergedRecommendedBooks
             .filter((_, index) => index < limitSize)
             .map((book) => (
               <BookCard
@@ -41,7 +89,7 @@ export const Recommendations = (props: RecommendationsType) => {
               />
             ))
         ) : (
-          recommendedBooks
+          mergedRecommendedBooks
             .map((book) => (
               <BookCard
                 key={book.id}
